34904d20e964b572b13bda0f3927939e3262bea5
Kevin Fowlks <fowlk1kd@gmail.com>
2016-10-08 16:18:08 -0400

    Updated convert.sh
    Added step to rename new file to old name so Plex would copy it to the correct directory.

--- convert.sh
+++ convert.sh
@@ -24,9 +24,15 @@ check_errs()
 BASEPATH=$(dirname "$1")
 BASEFILE=$(basename "$1")
 FILENAME="${BASEFILE%%.*}"
+
 echo "Starting $NOW"
 $PGMNAME -i "$1" -o "$BASEPATH/$FILENAME.mp4" -e x264 -q22 -B 64 -O -X 720 –preset="Normal"
 check_errs $? "Failed to convert file $1"
 
+echo "Removing original file."
 rm -f "$1"
 check_errs $? "Failed to remove original file $1"
+
+echo "Renaming new video to old name"
+mv "$BASEPATH/$FILENAME.mp4" "$1"
+check_errs $? "Failed to rename new file to original file name $1"
